34ac821ec30ffbc81029e145a492b4923d449bd1
ModjoInc <habibem@gmail.com>
2017-10-27 12:01:30 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isChecked = true;
+  colors = [
+    { id: 1, name: 'black'},
+    { id: 2, name: 'red'},
+    { id: 3, name: 'white'}
+  ];
+  color = 2;
+  minDate = new Date(2017, 1, 1);
+  maxDate = new Date(2017, 11, 1);
+  public myDate = new Date();
+  levels = [
+    {id: 1, name: 'Beginner'},
+    {id: 2, name: 'Intermediate'},
+    {id: 3, name: 'Advanced'}
+  ]
+
+  myFilter = (d: Date): boolean => {
+    const day = d.getDay();
+    const currentTime = new Date();
+
+    return day !== 0 && day !== 6;
+  }
+
+  selectLevels(level) {
+    this.levels
+    .filter(c => c !== level)
+    .forEach(c => c['selected'] = false);
+
+    level.selected = !level.selected;
+  }
+
+  onChange($event) {
+    console.log($event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  // tslint:disable-next-line:max-line-length
+  MatNativeDateModule, MatIconModule, MatDatepickerModule, MatSelectModule,  MatCheckboxModule, MatRadioModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatChipsModule
+} from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SassComponent } from './sass/sass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SassComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
